d93e8302f641b17f64265ffea2b84632976cc346
Mehedi <mehedi25122003@gmail.com>
2026-05-04 15:00:45 +0600

    thanks to claude to fix this daisyui issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
+import './index.css'
 import router from './router/router';
 
 ReactDOM.createRoot(root).render(
