fd0b8ed3997daa4e6bbc05b6aed8704f169beff4
Mehedi <mehedi25122003@gmail.com>
2026-05-04 10:33:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </section>
+    </>
+  )
+}
+
+export default App
